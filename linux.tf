69e43fd0f824a7d33ab0f4d108d023db3ad61773
robsonngr <robsonngr@gmail.com>
2022-05-02 23:17:12 -0300

    Criados arquivos output.tf e playbook, inventario e pagina html de exemplo

--- linux.tf
+++ linux.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "linux" {
-  count         = 1
+  count         = 2
   ami           = var.amis["us-east-1"]
   instance_type = var.instance_type
   key_name      = var.key_name
@@ -8,4 +8,8 @@ resource "aws_instance" "linux" {
     "Group" = "Linux"
   }
   vpc_security_group_ids = ["${aws_security_group.sg_servidoresweb.id}"]
+
+  provisioner "local-exec" {
+    command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook_linux_servers.yml -i inventory_linux.aws_ec2.yml"
+  }
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,19 @@
 variable "regiao" {
   default = "us-east-1"
 }
+
+variable "amis" {
+  type = map(any)
+
+  default = {
+    "us-east-1" = "ami-0c4f7023847b90238"
+  }
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "chave_privada_pem"
+}
