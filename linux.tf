66836cbd998777468cf9063cc8e46e41bb7f0f42
robsonngr <robsonngr@gmail.com>
2022-05-05 05:19:23 -0300

    Alterado nome do inventario no provisioner

--- linux.tf
+++ linux.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "linux" {
   vpc_security_group_ids = ["${aws_security_group.sg_servidoresweb.id}"]
 
   #provisioner "local-exec" {
-  #  command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook_linux_servers.yml -i inventory_linux.aws_ec2.yml"
+  #  command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook_linux_servers.yml -i inventory.aws_ec2.yml"
   #}
 }
