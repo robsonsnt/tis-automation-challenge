ef811e3da379ee7aa4d253e487a52f4d8678ff61
robsonngr <robsonngr@gmail.com>
2022-05-05 04:25:26 -0300

    Comentado bloco do provisioner

--- linux.tf
+++ linux.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "linux" {
-  count         = 2
+  count         = 1
   ami           = var.amis["us-east-1"]
   instance_type = var.instance_type
   key_name      = var.key_name
@@ -9,7 +9,7 @@ resource "aws_instance" "linux" {
   }
   vpc_security_group_ids = ["${aws_security_group.sg_servidoresweb.id}"]
 
-  provisioner "local-exec" {
-    command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook_linux_servers.yml -i inventory_linux.aws_ec2.yml"
-  }
+  #provisioner "local-exec" {
+  #  command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook playbook_linux_servers.yml -i inventory_linux.aws_ec2.yml"
+  #}
 }
