ea9236d07bc393bba633eedc30cbc53af17c970e
robsonngr <robsonngr@gmail.com>
2022-05-05 04:25:18 -0300

    Comentado bloco do provisioner

--- windows.tf
+++ windows.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "windows" {
   count         = 1
   instance_type = var.instance_type
   key_name      = var.key_name
-  #vpc_security_group_ids =["sg-fdb42694"]
   get_password_data = "true"
   user_data     = <<EOF
 <powershell>
@@ -19,6 +18,8 @@ Set-Item -Path WSMan:\localhost\Service\Auth\Certificate -Value $true
 # Set Administrator password
 $admin = [adsi]("WinNT://./administrator, user")
 $admin.psbase.invoke("SetPassword", "${var.admin_password}")
+# Habilitar descoberta de rede
+Get-NetFirewallRule -DisplayGroup 'Network Discovery'|Set-NetFirewallRule -Profile 'Private, Domain' -Enabled true
 </powershell>
 EOF
   tags = {
@@ -26,4 +27,8 @@ EOF
     "Group" = "Windows"
   }
   vpc_security_group_ids = ["${aws_security_group.sg_servidoresweb.id}"]
+
+  #provisioner "local-exec" {
+  #  command = "sleep 60 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i inventory.aws_ec2.yml configura_windows.yml -e ansible_password='{{ lookup("env", "TF_VAR_admin_password") }}'"
+  #}
 }
