705ed2681beb2cfa5d81a4513aa481b339f20f97
robsonngr <robsonngr@gmail.com>
2022-05-05 05:15:26 -0300

    Alterado para backend remoto

--- backend.tf
+++ backend.tf
@@ -1,5 +1,13 @@
+#terraform {
+#  backend "local" {
+#      path = "terraform.tfstate"
+#  }
+#}
+
 terraform {
-  backend "local" {
-      path = "terraform.tfstate"
+  backend "s3" {
+    region = "us-east-1" # Substitua o valor pela região onde criou o bucket
+    bucket = "NOME_DO_BUCKET" # Substitua pelo nome do bucket criado
+    key    = "tfstate"
   }
 }
