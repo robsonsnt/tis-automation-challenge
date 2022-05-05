7d984b270f6878755d797e3cf86e3508f8b0f776
robsonngr <robsonngr@gmail.com>
2022-05-05 04:17:33 -0300

    incluido output para hosts windows

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
-output "public_ip" {
+output "public_ip_linux" {
     value = "${aws_instance.linux.*.public_ip}"
 }
 
-output "public_dns" {
+output "public_dns_linux" {
     value = "${aws_instance.linux.*.public_dns}"
+}
+
+output "public_ip_windows" {
+    value = "${aws_instance.windows.*.public_ip}"
+}
+
+output "public_dns_windows" {
+    value = "${aws_instance.windows.*.public_dns}"
 }
