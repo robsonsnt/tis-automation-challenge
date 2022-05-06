7fee84020efe92749e86507587c071aa4e2fe25d
robsonsnt <robsonsnt>
2022-05-06 20:56:09 -0300

    Alterado arquivo de backend para realizaçao de testes

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@
 terraform {
   backend "s3" {
     region = "us-east-1" # Substitua o valor pela região onde criou o bucket
-    bucket = "NOME_DO_BUCKET" # Substitua pelo nome do bucket criado
+    bucket = "tis-automation-bucket" # Substitua pelo nome do bucket criado
     key    = "tfstate"
   }
 }
